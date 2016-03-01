0c6c5a3945b492c5275d01d1533597ce08c66bc5
derreck <mansheim23@hotmail.com>
2016-03-01 10:48:14 -0600

    LinkedList.rb version 2.0

--- lib/LinkedList.rb
+++ lib/LinkedList.rb
@@ -1,35 +1,53 @@
 class LinkedList
-  def initialize(value)
-    @head = Node.new(value, nil)
-  end
-
-  def get_value_at(index)
-    if @head
-      node.head
-    else
-      index.times do
-        node.link
-      end
+  #class Node is private only to class LinkedList
+  class Node
+    def initialize(data, link)
+      @data = data
+      @link = link
     end
-    node.data
+    def data
+      @data
+    end
+    def link
+      @link
+    end
   end
 
+  def initialize
+    @head = nil
+  end
 
-def <<(new_data)
-  n = Node.new(new_data)
-  n.next = @head
-  @head = n
-end
+  def unshift(data)
+    @head = Node.new(data, @head)
+    self
+  end
 
-def shift_value_in_linked_list
-  node_being_returned = @head
-  @head = @head.link
-  node_being_returned
-end
+  def shift
+    return nil unless @head
+    data = @head.data
+    @head = @head.link
+    data
+  end
 
-def pop_value_in_linked_list
-  @head = @head.link
-end
-                                                                                                end
+  def [](index)
+    return nil unless @head
+    #local variable node, instance variable head
+    node = @head
+    while node && index > 0
+      node = node.link
+      index -= 1
+    end
+    node && node.data
   end
 end
+
+list = LinkedList.new
+list.unshift('c').unshift('b').unshift('a')
+list[0] # => "a"
+list[1] # => "b"
+list[2] # => "c"
+list[3] # => nil
+list.shift # => "a"
+list.shift # => "b"
+list.shift # => "c"
+list.shift # => nil
